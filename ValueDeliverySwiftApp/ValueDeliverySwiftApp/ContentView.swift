d1edaa8152ecba138b370ab5883a2aac8527ce65
selfstudymaster <webselfmaster@gmail.com>
2020-12-19 11:35:34 +0900

    This is the first commit.

---
+++ ValueDeliverySwiftApp/ValueDeliverySwiftApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ValueDeliverySwiftApp
+//
+//  Created by mac on 2020/12/19.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
