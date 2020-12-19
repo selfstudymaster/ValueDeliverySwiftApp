d030d58e9b91ff93713de8db34ffa33a9c3cb0b7
selfstudymaster <webselfmaster@gmail.com>
2020-12-19 21:41:26 +0900

    Add NavigationView and NavigationLink

--- ValueDeliverySwiftApp/ValueDeliverySwiftApp/ContentView.swift
+++ ValueDeliverySwiftApp/ValueDeliverySwiftApp/ContentView.swift
@@ -1,21 +1,33 @@
-//
-//  ContentView.swift
-//  ValueDeliverySwiftApp
-//
-//  Created by mac on 2020/12/19.
-//  Copyright © 2020 mac. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name = ""
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            VStack(spacing: 30.0) {
+                TextField("名前を入力してください。", text: $name)
+                    .border(Color.blue, width: 2)
+                    .font(.title)
+                    .padding(.horizontal, 20.0)
+                NavigationLink(destination: SubView(name: $name)) {
+                    Text("Enter")
+                        .font(.title)
+                }
+                
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct SubView: View {
+    
+    @Binding var name: String
+    
+    var body: some View {
+        Text("はじめまして、 \(name)さん")
+            .font(.largeTitle)
+            .foregroundColor(Color.pink)
     }
 }
